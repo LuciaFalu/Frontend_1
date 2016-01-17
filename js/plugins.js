05922a33bbd091e85c3c5698ffa1eef0fc788f0d
LuciaFalu <luciafalu.2509@gmail.com>
2016-01-17 16:22:02 -0500

    Reto1_FRONTEND

---
+++ js/plugins.js
@@ -0,0 +1,32 @@
+// Avoid `console` errors in browsers that lack a console.
+/*$(document).on("ready",configuracionApp);
+
+function configuracionApp(){
+    var canvas=document.getElementById("miCanvas");
+    var ctx=canvas.getContext("2d");
+    canvas.width=screen.availWidth;
+}*/
+
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd','timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
